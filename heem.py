124f59ad9aa62fa3b39a6794666da945303ceebb
kyosuke53 <94458271+kyosuke53@users.noreply.github.com>
2021-11-16 20:37:11 +0700

    Create heem.py

---
+++ heem.py
@@ -0,0 +1,11 @@
+Nama = "Alvin Mumtaz"
+nim = "212410102064"
+Prodi = "Teknologi Informasi"
+Asal = "Banyuwangi"
+
+
+
+print("Nama :", Nama)
+print("nim :", nim)
+print("Prodi :", Prodi)
+print("Asal :", Asal)
